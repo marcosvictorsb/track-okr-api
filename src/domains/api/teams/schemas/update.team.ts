31b87930b4383310cef49eb8700ce18d4009bc42
marcos <marcovictorsb@gmail.com>
2025-06-30 15:29:23 -0300

    refactor: remove amount_users validation from update team schema

--- src/domains/api/teams/schemas/update.team.ts
+++ src/domains/api/teams/schemas/update.team.ts
@@ -16,11 +16,6 @@ export const updateTeamSchema = z.object({
     description: z
       .string()
       .min(1, 'Descrição é obrigatória')
-      .max(250, 'A descrição deve ter no máximo 250 caracteres'),
-    amount_users: z
-      .number()
-      .int()
-      .min(1, 'O time deve ter pelo menos 1 usuário')
-      .max(50, 'O time pode ter no máximo 50 usuários')
+      .max(250, 'A descrição deve ter no máximo 250 caracteres')
   })
 });
