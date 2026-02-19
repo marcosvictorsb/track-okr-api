d2f36e2b8300eb657608f676106106052033184e
marcos <marcosvictorsb@gmail.com>
2026-02-19 11:00:27 -0300

    refactor: clean up response mock implementation and remove unused interface

--- src/test/mocks/response.mocks.ts
+++ src/test/mocks/response.mocks.ts
@@ -1,13 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Response } from 'express';
 import { vi } from 'vitest';
 
-interface Response {
-  status?: any;
-  json?: any;
-}
+// interface Response {
+//   status?: any;
+//   json?: any;
+// }
 
-export function makeResponseMock() {
-  const res: Response = {};
+export function makeResponseMock(): Response {
+  const res: Response = {} as Response;
   res.status = vi.fn().mockReturnValue(res);
   res.json = vi.fn().mockReturnValue(res);
   return res;
